8500070de671e09e6297114f36dbd55da671e057
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-11-30 12:04:35 +0900

    Reentrancy test to check how the two banks handle the attack

---
+++ test/reentrancy.test.js
@@ -0,0 +1,101 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Reentrancy Vulnerability Lab", function () {
+    async function deployReentrancyFixture() {
+        const [deployer, user, attackerEOA] = await ethers.getSigners();
+
+        const VulnerableBank = await ethers.getContractFactory("VulnerableBank");
+        const FixedBank = await ethers.getContractFactory("FixedBank");
+        const Attacker = await ethers.getContractFactory("Attacker");
+
+        const vulnerableBank = await VulnerableBank.deploy();
+        await vulnerableBank.waitForDeployment();
+
+        const fixedBank = await FixedBank.deploy();
+        await fixedBank.waitForDeployment();
+
+        const attackerAgainstVulnerable = await Attacker.connect(attackerEOA).deploy(
+            await vulnerableBank.getAddress()
+        );
+        await attackerAgainstVulnerable.waitForDeployment();
+
+        const attackerAgainstFixed = await Attacker.connect(attackerEOA).deploy(
+            await fixedBank.getAddress()
+        );
+        await attackerAgainstFixed.waitForDeployment();
+
+        const initialUserDeposit = ethers.parseEther("10");
+        await vulnerableBank.connect(user).deposit({ value: initialUserDeposit });
+        await fixedBank.connect(user).deposit({ value: initialUserDeposit });
+
+        return {
+            deployer,
+            user,
+            attackerEOA,
+            vulnerableBank,
+            fixedBank,
+            attackerAgainstVulnerable,
+            attackerAgainstFixed,
+            initialUserDeposit,
+        };
+    }
+
+    it("allows attacker to drain the VulnerableBank using reentrancy", async function () {
+        const {
+            attackerEOA,
+            vulnerableBank,
+            attackerAgainstVulnerable,
+        } = await deployReentrancyFixture();
+
+        const bankAddress = await vulnerableBank.getAddress();
+        const attackerContractAddress = await attackerAgainstVulnerable.getAddress();
+
+        const bankBalanceBefore = await ethers.provider.getBalance(bankAddress);
+        expect(bankBalanceBefore).to.equal(ethers.parseEther("10"));
+
+        const attackDeposit = ethers.parseEther("1");
+        await attackerAgainstVulnerable
+            .connect(attackerEOA)
+            .attack({ value: attackDeposit });
+
+        const bankBalanceAfter = await ethers.provider.getBalance(bankAddress);
+        const attackerContractBalance = await ethers.provider.getBalance(
+            attackerContractAddress
+        );
+
+        expect(bankBalanceAfter).to.equal(0n);
+
+        expect(attackerContractBalance).to.be.greaterThanOrEqual(
+            ethers.parseEther("10")
+        );
+    });
+
+    it("blocks the same attack against FixedBank", async function () {
+        const {
+            attackerEOA,
+            fixedBank,
+            attackerAgainstFixed,
+        } = await deployReentrancyFixture();
+
+        const bankAddress = await fixedBank.getAddress();
+        const attackerContractAddress = await attackerAgainstFixed.getAddress();
+
+        const bankBalanceBefore = await ethers.provider.getBalance(bankAddress);
+        expect(bankBalanceBefore).to.equal(ethers.parseEther("10"));
+
+        const attackDeposit = ethers.parseEther("1");
+
+        await expect(
+            attackerAgainstFixed.connect(attackerEOA).attack({ value: attackDeposit })
+        ).to.not.be.reverted;
+
+        const bankBalanceAfter = await ethers.provider.getBalance(bankAddress);
+        const attackerContractBalance = await ethers.provider.getBalance(
+            attackerContractAddress
+        );
+
+        expect(bankBalanceAfter).to.be.greaterThan(0n);
+        expect(attackerContractBalance).to.be.lessThan(bankBalanceBefore);
+    });
+});
