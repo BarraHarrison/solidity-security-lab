6ea6a0243a96853628153625b6a9a12013c8d8e1
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-12-03 10:57:22 +0900

    keccak256 encrypts the guess and makes it secure from attacks

---
+++ contracts/frontrunning/GuessSecure.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.24;
+
+contract GuessSecure {
+    uint256 public rewardPool;
+    address public winner;
+
+    mapping(address => bytes32) public commitments;
+
+    constructor() payable {
+        rewardPool = msg.value;
+    }
+
+    function commit(bytes32 hash) external {
+        commitments[msg.sender] = hash;
+    }
+
+    function reveal(uint256 guess, uint256 secret, uint256 answer) external {
+        bytes32 expected = keccak256(abi.encodePacked(secret, guess));
+        require(commitments[msg.sender] == expected, "bad reveal");
+
+        if (guess == answer && winner == address(0)) {
+            winner = msg.sender;
+            uint256 amount = rewardPool;
+            rewardPool = 0;
+            (bool ok,) = msg.sender.call{value: amount}("");
+            require(ok, "fail");
+        }
+    }
+}
