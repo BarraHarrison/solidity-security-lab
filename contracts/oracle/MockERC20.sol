5212ee8a10e399a71e7160d4c32d203614354b2e
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-12-07 11:55:29 +0900

    ERC20-Mock-Token needed for the program to run

---
+++ contracts/oracle/MockERC20.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract MockERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory n, string memory s, uint8 d) {
+        name = n;
+        symbol = s;
+        decimals = d;
+    }
+
+    function mint(address to, uint256 amt) external {
+        balanceOf[to] += amt;
+    }
+
+    function transfer(address to, uint256 amt) external returns (bool) {
+        require(balanceOf[msg.sender] >= amt, "not enough balance");
+        balanceOf[msg.sender] -= amt;
+        balanceOf[to] += amt;
+        return true;
+    }
+}
