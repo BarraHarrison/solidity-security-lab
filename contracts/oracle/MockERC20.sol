adb69b4ec9784f613d32340fe18b0258e68426c1
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-12-07 12:05:09 +0900

    Mock ERC20-Token functions added to meet minimal ERC20 requirements

--- contracts/oracle/MockERC20.sol
+++ contracts/oracle/MockERC20.sol
@@ -4,24 +4,41 @@ pragma solidity ^0.8.24;
 contract MockERC20 {
     string public name;
     string public symbol;
-    uint8 public decimals;
+    uint8 public decimals = 18;
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(string memory n, string memory s, uint8 d) {
-        name = n;
-        symbol = s;
-        decimals = d;
+    constructor(string memory _name, string memory _symbol, address initialHolder, uint256 initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        balanceOf[initialHolder] = initialSupply;
     }
 
-    function mint(address to, uint256 amt) external {
-        balanceOf[to] += amt;
-    }
-
-    function transfer(address to, uint256 amt) external returns (bool) {
-        require(balanceOf[msg.sender] >= amt, "not enough balance");
-        balanceOf[msg.sender] -= amt;
-        balanceOf[to] += amt;
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
         return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(balanceOf[from] >= amount, "insufficient balance");
+        require(allowance[from][msg.sender] >= amount, "insufficient allowance");
+
+        allowance[from][msg.sender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+
+        return true;
+    }
+
+    function mint(address to, uint256 amount) external {
+        balanceOf[to] += amount;
     }
 }
